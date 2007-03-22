783ec4fd1eaf59cb990f213d8aa2751e93fa086f
Robert Iakobashvili <coroberti@gmail.com>
2007-03-22 06:11:06 +0000

    GPL license added.

--- curl-loader/ssl_thr_lock.c
+++ curl-loader/ssl_thr_lock.c
@@ -1,13 +1,27 @@
 /* 
-   ssl_thr_lock.c
-
-   Taken with thanks from CURL project opensslthreadlock.c by 
-   Jeremy Brown <jbrown_at_novell.com> 
-   and modified by 
-   Robert Iakobashvili <coroberti@gmail.com>
+*   ssl_thr_lock.c
+*
+*   Author: Jeremy Brown <jbrown_at_novell.com> 
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*
+  Style changes by Robert Iakobashvili <coroberti@gmail.com>.
 */
 
-// must be first include
+// must be the first include
 #include "fdsetsize.h"
 
 #include <pthread.h>
