45b0d7340d20114f454e774638da1d1765559480
Robert Iakobashvili <coroberti@gmail.com>
2008-01-05 11:42:35 +0000

    Adaptation for POST-ing binary file

--- curl-loader/loader.c
+++ curl-loader/loader.c
@@ -710,11 +710,11 @@ int setup_curl_handle_appl (client_context*const cctx, url_context* url)
           /* 
              Make POST, using post buffer, if requested. 
           */
-            if (url->upload_file && url->upload_file_ptr && !cctx->post_data)
+            if (url->upload_file && url->upload_file_ptr && (!cctx->post_data || !cctx->post_data[0]))
             {
                 curl_easy_setopt(handle, CURLOPT_POST, 1);
             }
-            else if (cctx->post_data)
+            else if (cctx->post_data && cctx->post_data[0])
             {
               /* 
                  Sets POST as the HTTP request method using either:
